2056380763ded5912a69363dfc9eb326373ae80a
Antonio Silva <antonios.carlos@hotmail.com>
2020-07-03 00:50:13 -0300

    Update handler.js

--- desafio3/sum-to-number/handler.js
+++ desafio3/sum-to-number/handler.js
@@ -2,23 +2,11 @@
 
 module.exports.soma = (event, context) => {
 
-  let title = event.pathParameters.title;
-  let body = JSON.parse(event.body);
-  let valor1 = body.a;
-  let valor2 = body.b;
-
+  let valor1 = event.a;
+  let valor2 = event.b;
+  let result = valor1 + valor2;
   return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: valor1 + valor2,
-        input: event,
-      },
-      null,
-      2
-    ),
+        resultado: result
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  // return { resultado: valor1 + valor2 };
 };
